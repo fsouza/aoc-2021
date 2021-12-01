9995ce0642f342ec856d9780dd301ef685eba4d1
francisco souza <fsouza@users.noreply.github.com>
2021-12-01 00:50:24 -0500

    day01: use `as` instead of recreating the list

--- day01/part1.ml
+++ day01/part1.ml
@@ -3,8 +3,8 @@ let read_input () = Aoc.stdin |> Seq.map int_of_string |> List.of_seq
 let count_increases =
   let rec count_increases' acc = function
     | [] | [ _ ] -> acc
-    | first :: second :: tl when second > first ->
-        count_increases' (acc + 1) (second :: tl)
+    | first :: (second :: _ as tl) when second > first ->
+        count_increases' (acc + 1) tl
     | _ :: tl -> count_increases' acc tl
   in
   count_increases' 0
--- day01/part2.ml
+++ day01/part2.ml
@@ -3,10 +3,9 @@ let read_input () = Aoc.stdin |> Seq.map int_of_string |> List.of_seq
 let count_increases =
   let rec count_increases' acc = function
     | [] | [ _ ] | [ _; _ ] | [ _; _; _ ] -> acc
-    | first :: second :: third :: fourth :: tl ->
+    | first :: (second :: third :: fourth :: _ as tl) ->
         let prev_window = first + second + third in
         let this_window = second + third + fourth in
-        let tl = second :: third :: fourth :: tl in
         if this_window > prev_window then count_increases' (acc + 1) tl
         else count_increases' acc tl
   in
