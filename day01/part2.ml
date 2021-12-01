fe15740d0caf08f4953bfa5b25128de121f64b87
francisco souza <fsouza@users.noreply.github.com>
2021-12-01 00:47:36 -0500

    day01: part2 with a list
    idk, just playing around

--- day01/part2.ml
+++ day01/part2.ml
@@ -1,13 +1,15 @@
-let read_input () = Aoc.stdin |> Seq.map int_of_string |> Array.of_seq
+let read_input () = Aoc.stdin |> Seq.map int_of_string |> List.of_seq
 
-let window_increases arr =
-  let length = Array.length arr in
-  let increases = ref 0 in
-  for i = 1 to length - 3 do
-    let prev_window = arr.(i - 1) + arr.(i) + arr.(i + 1) in
-    let this_window = arr.(i) + arr.(i + 1) + arr.(i + 2) in
-    if this_window > prev_window then incr increases
-  done;
-  !increases
+let count_increases =
+  let rec count_increases' acc = function
+    | [] | [ _ ] | [ _; _ ] | [ _; _; _ ] -> acc
+    | first :: second :: third :: fourth :: tl ->
+        let prev_window = first + second + third in
+        let this_window = second + third + fourth in
+        let tl = second :: third :: fourth :: tl in
+        if this_window > prev_window then count_increases' (acc + 1) tl
+        else count_increases' acc tl
+  in
+  count_increases' 0
 
-let () = read_input () |> window_increases |> Printf.printf "%d\n"
+let () = read_input () |> count_increases |> Printf.printf "%d\n"
