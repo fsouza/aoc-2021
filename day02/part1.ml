46a03785ba606103862d77f564236b089ae5bbef
francisco souza <fsouza@users.noreply.github.com>
2021-12-02 07:03:44 -0500

    day02/part1: use option + filter_map

--- day02/part1.ml
+++ day02/part1.ml
@@ -2,10 +2,10 @@ open StdLabels
 
 let parse line =
   match String.split_on_char ~sep:' ' line with
-  | [ "forward"; v ] -> (int_of_string v, 0)
-  | [ "down"; v ] -> (0, int_of_string v)
-  | [ "up"; v ] -> (0, -1 * int_of_string v)
-  | _ -> line |> Printf.sprintf "invalid input '%s'" |> invalid_arg
+  | [ "forward"; v ] -> Some (int_of_string v, 0)
+  | [ "down"; v ] -> Some (0, int_of_string v)
+  | [ "up"; v ] -> Some (0, -1 * int_of_string v)
+  | _ -> None
 
 let execute (horizontal, depth) (x, y) = (horizontal + x, depth + y)
 
@@ -13,7 +13,7 @@ let mult (horizontal, depth) = horizontal * depth
 
 let () =
   Aoc.stdin
-  |> Seq.map parse
+  |> Seq.filter_map parse
   |> Seq.fold_left execute (0, 0)
   |> mult
   |> Printf.printf "%d\n"
