9136f55a5918dbc81cbd0c2f89050bac180e7321
francisco souza <fsouza@users.noreply.github.com>
2021-12-03 00:44:25 -0500

    day03/part2: some refactoring

--- day03/part2.ml
+++ day03/part2.ml
@@ -2,11 +2,6 @@ open StdLabels
 
 let pow2 x = x |> float_of_int |> ( ** ) 2. |> int_of_float
 
-let to_int = function
-  | '1' -> 1
-  | '0' -> 0
-  | _ -> invalid_arg "invalid digit passed to `to_int`"
-
 let to_dec =
   let rec to_dec' acc exp = function
     | [] -> acc
@@ -14,48 +9,47 @@ let to_dec =
   in
   to_dec' 0 0
 
-let oxygen_generator_rating input =
-  let max_col =
-    match input with
-    | [] -> 0
-    | hd :: _ -> Array.length hd
+let dominant input col =
+  let zeroes, ones =
+    List.fold_left ~init:(0, 0)
+      ~f:(fun (zeroes, ones) row ->
+        if row.(col) = 1 then (zeroes, ones + 1) else (zeroes + 1, ones))
+      input
   in
+  if zeroes > ones then Some 0 else if ones > zeroes then Some 1 else None
+
+let oxygen_generator_rating input =
   let rec loop acc col =
-    if col = max_col then acc |> List.hd |> Array.to_list |> List.rev |> to_dec
+    if List.length acc = 1 then
+      acc |> List.hd |> Array.to_list |> List.rev |> to_dec
     else
-      let zeroes, ones =
-        List.fold_left ~init:(0, 0)
-          ~f:(fun (zeroes, ones) row ->
-            if row.(col) = 1 then (zeroes, ones + 1) else (zeroes + 1, ones))
-          acc
-      in
-      if zeroes > ones then
-        loop (List.filter ~f:(fun entry -> entry.(col) = 0) acc) (col + 1)
-      else loop (List.filter ~f:(fun entry -> entry.(col) = 1) acc) (col + 1)
+      let val_to_keep = dominant acc col |> Option.value ~default:1 in
+      loop
+        (List.filter ~f:(fun entry -> entry.(col) = val_to_keep) acc)
+        (col + 1)
   in
   loop input 0
 
 let co2_scrubber_rating input =
-  let max_col =
-    match input with
-    | [] -> 0
-    | hd :: _ -> Array.length hd
-  in
   let rec loop acc col =
-    if col = max_col || List.length acc = 1 then
+    if List.length acc = 1 then
       acc |> List.hd |> Array.to_list |> List.rev |> to_dec
     else
-      let zeroes, ones =
-        List.fold_left ~init:(0, 0)
-          ~f:(fun (zeroes, ones) row ->
-            if row.(col) = 1 then (zeroes, ones + 1) else (zeroes + 1, ones))
-          acc
+      let val_to_keep =
+        dominant acc col
+        |> Option.map (fun v -> if v = 0 then 1 else 0)
+        |> Option.value ~default:0
       in
-      if zeroes > ones then
-        loop (List.filter ~f:(fun entry -> entry.(col) = 1) acc) (col + 1)
-      else loop (List.filter ~f:(fun entry -> entry.(col) = 0) acc) (col + 1)
+      loop
+        (List.filter ~f:(fun entry -> entry.(col) = val_to_keep) acc)
+        (col + 1)
   in
   loop input 0
+
+let to_int = function
+  | '1' -> 1
+  | '0' -> 0
+  | _ -> invalid_arg "invalid digit passed to `to_int`"
 
 let () =
   let input =
