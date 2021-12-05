0e0d2570a3609011ec0516d70c1335804f86f1fb
francisco souza <fsouza@users.noreply.github.com>
2021-12-05 17:52:23 -0500

    aoc: add some helper functions for dealing with sequences

--- aoc.ml
+++ aoc.ml
@@ -4,3 +4,17 @@ let rec stdin () =
     let line = read_line () in
     Cons (line, stdin)
   with End_of_file -> Nil
+
+let rec take n seq () =
+  let open Seq in
+  if n = 0 then Nil
+  else
+    match seq () with
+    | Nil -> Nil
+    | Cons (v, seq) -> Cons (v, take (n - 1) seq)
+
+let rec zip seq1 seq2 () =
+  let open Seq in
+  match (seq1 (), seq2 ()) with
+  | Cons (v1, seq1), Cons (v2, seq2) -> Cons ((v1, v2), zip seq1 seq2)
+  | _ -> Nil
--- aoc.mli
+++ aoc.mli
@@ -1,1 +1,5 @@
 val stdin : string Seq.t
+
+val take : int -> 'a Seq.t -> 'a Seq.t
+
+val zip : 'a Seq.t -> 'b Seq.t -> ('a * 'b) Seq.t
