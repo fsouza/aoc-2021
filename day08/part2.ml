19bb0bbc5c7642410f5d39289a259781fb26a8a6
francisco souza <fsouza@users.noreply.github.com>
2021-12-08 13:34:05 -0500

    day08/part2: generalized solution
    It's what I did on the paper, but algorithmically.

--- day08/part2.ml
+++ day08/part2.ml
@@ -1,5 +1,6 @@
 open StdLabels
 open MoreLabels
+module CharMap = Map.Make (Char)
 module CharSet = Set.Make (Char)
 module IntMap = Map.Make (Int)
 
@@ -11,30 +12,7 @@ let parse str =
   | _ -> None
 
 let all_chars = CharSet.of_list [ 'a'; 'b'; 'c'; 'd'; 'e'; 'f'; 'g' ]
-
 let charset_of_string str = str |> String.to_seq |> CharSet.of_seq
-
-(* Note: I derived this with a piece of paper. Perhaps I could automate it?
-   Have an algorithm figure out the rules? Use something other than sets? idk
-   Will investigate :)
-
-   7 - 1 = a
-   0 & 1 & 6 & 9 = f
-   1 = c | f, I know f, so now I know c
-   2 & 3 & 5 & 4 = d
-   2 & 3 & 5 - a - d = g
-   4 - 1 = b | d, I know d, so I know b
-   if I know a, b, c, d, f and g, I know e :)
-*)
-
-(*
-  6 digits -> 0 or 6 or 9
-  2 digits -> 1
-  3 digits -> 7
-  4 digits -> 4
-  5 digits -> 2 or 3 or 5
-  7 digits -> 8
-*)
 
 let regroup_by_length =
   List.fold_left ~init:IntMap.empty ~f:(fun map pattern ->
@@ -42,42 +20,61 @@ let regroup_by_length =
       let curr = IntMap.find_opt length map |> Option.value ~default:[] in
       IntMap.add ~key:length ~data:(charset_of_string pattern :: curr) map)
 
-let analyze patterns =
-  let result = Hashtbl.create 7 in
-  let grouped = regroup_by_length patterns in
-  let seven = grouped |> IntMap.find 3 |> List.hd in
-  let one = grouped |> IntMap.find 2 |> List.hd in
-  let four = grouped |> IntMap.find 4 |> List.hd in
-  let a = CharSet.diff seven one in
-  let abfg =
-    grouped |> IntMap.find 6 |> List.fold_left ~init:all_chars ~f:CharSet.inter
+let is_singleton s = CharSet.cardinal s = 1
+
+let resolve input =
+  let sets =
+    input
+    |> regroup_by_length
+    |> IntMap.fold ~init:[] ~f:(fun ~key:_ ~data acc ->
+           List.fold_left ~init:all_chars ~f:CharSet.inter data :: acc)
+    |> Array.of_list
   in
-  let f = CharSet.inter abfg one in
-  let c = CharSet.diff one f in
-  let adg =
-    grouped |> IntMap.find 5 |> List.fold_left ~init:all_chars ~f:CharSet.inter
+  let advance i j =
+    let j = j + 1 in
+    if j = Array.length sets then (i + 1, i + 2) else (i, j)
   in
-  let d = CharSet.inter adg four in
-  let g = a |> CharSet.union d |> CharSet.diff adg in
-  let bd = CharSet.diff four one in
-  let b = CharSet.diff bd d in
-  let e =
-    a
-    |> CharSet.union b
-    |> CharSet.union c
-    |> CharSet.union d
-    |> CharSet.union f
-    |> CharSet.union g
-    |> CharSet.diff all_chars
+  let rec loop acc i j =
+    flush stdout;
+    if List.length acc = 7 then acc
+    else if i = Array.length sets - 1 then
+      (* Note: this could loop forever on untrusted inputs, but the input from
+                AoC it's fine. If we couldn't
+                trust the input, I'd do something like checking that acc changed in
+                this attempt *)
+      loop acc 0 1
+    else
+      let acc_set =
+        List.fold_left ~init:CharSet.empty ~f:(Fun.flip CharSet.add) acc
+      in
+      let s = CharSet.diff sets.(i) sets.(j) in
+      let s = acc_set |> CharSet.diff s in
+      let i, j = advance i j in
+      if is_singleton s then
+        let ch = CharSet.min_elt s in
+        loop (ch :: acc) i j
+      else loop acc i j
   in
-  Hashtbl.add ~key:(CharSet.min_elt a) ~data:'a' result;
-  Hashtbl.add ~key:(CharSet.min_elt b) ~data:'b' result;
-  Hashtbl.add ~key:(CharSet.min_elt c) ~data:'c' result;
-  Hashtbl.add ~key:(CharSet.min_elt d) ~data:'d' result;
-  Hashtbl.add ~key:(CharSet.min_elt e) ~data:'e' result;
-  Hashtbl.add ~key:(CharSet.min_elt f) ~data:'f' result;
-  Hashtbl.add ~key:(CharSet.min_elt g) ~data:'g' result;
-  result
+  loop [] 0 1
+
+let real_data =
+  [
+    "abcefg";
+    "cf";
+    "acdeg";
+    "acdfg";
+    "bcdf";
+    "abdfg";
+    "abdefg";
+    "acf";
+    "abcdefg";
+    "abcdfg";
+  ]
+  |> resolve
+
+let get_mapping patterns =
+  let solution = resolve patterns in
+  List.combine solution real_data |> List.to_seq |> CharMap.of_seq
 
 let get_digit = function
   | "abcefg" -> Some '0'
@@ -93,13 +90,13 @@ let get_digit = function
   | _ -> None
 
 let resolve (patterns, output) =
-  let mapping = analyze patterns in
+  let mapping = get_mapping patterns in
   output
   |> List.to_seq
   |> Seq.map (fun str ->
          str
          |> String.to_seq
-         |> Seq.map (Hashtbl.find mapping)
+         |> Seq.map ((Fun.flip CharMap.find) mapping)
          |> List.of_seq
          |> List.sort ~cmp:Char.compare
          |> List.to_seq
