fb44fb25c4a9c8920fcbf5d21978fc5684da32cc
francisco souza <fsouza@users.noreply.github.com>
2021-12-09 00:26:14 -0500

    Add a sequence of natural numbers to the shared library

--- aoc.ml
+++ aoc.ml
@@ -1,12 +1,12 @@
+open Seq
+
 let rec stdin () =
-  let open Seq in
   try
     let line = read_line () in
     Cons (line, stdin)
   with End_of_file -> Nil
 
 let rec take n seq () =
-  let open Seq in
   if n = 0 then Nil
   else
     match seq () with
@@ -14,7 +14,10 @@ let rec take n seq () =
     | Cons (v, seq) -> Cons (v, take (n - 1) seq)
 
 let rec zip seq1 seq2 () =
-  let open Seq in
   match (seq1 (), seq2 ()) with
   | Cons (v1, seq1), Cons (v2, seq2) -> Cons ((v1, v2), zip seq1 seq2)
   | _ -> Nil
+
+let nat =
+  let rec nat' n () = Cons (n, nat' (n + 1)) in
+  nat' 0
--- aoc.mli
+++ aoc.mli
@@ -1,3 +1,4 @@
 val stdin : string Seq.t
 val take : int -> 'a Seq.t -> 'a Seq.t
 val zip : 'a Seq.t -> 'b Seq.t -> ('a * 'b) Seq.t
+val nat : int Seq.t
