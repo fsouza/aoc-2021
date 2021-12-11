103289b476f27534147e950300f45ad93d1fbd5c
francisco souza <fsouza@users.noreply.github.com>
2021-12-11 10:00:05 -0500

    Day 11 part 2

--- day11/day11.ml
+++ day11/day11.ml
@@ -72,36 +72,30 @@ let step grid =
   let flashed, grid = grid |> PosMap.map ~f:(( + ) 1) |> flash in
   (grid, PosSet.cardinal flashed)
 
-let print_grid step grid =
-  (* this is ugly *)
-  Printf.printf "After step %d:\n" step;
-  let curr_row = ref 0 in
-  grid
-  |> PosMap.to_seq
-  |> Seq.map fst
-  |> List.of_seq
-  |> List.sort ~cmp:Pos.compare
-  |> List.iter ~f:(fun ((row, _) as pos) ->
-         if row > !curr_row then (
-           print_newline ();
-           incr curr_row);
-         print_int (PosMap.find pos grid));
-  print_endline "\n";
-  flush stdout
+let count_runs_until f =
+  let rec run' runs flashed grid =
+    if f flashed then runs
+    else
+      let grid, flashed = step grid in
+      run' (runs + 1) flashed grid
+  in
+  run' 0 0
 
 let run orig_n grid =
-  print_grid 0 grid;
   let rec run' n_flashed n grid =
     if n = 0 then n_flashed
     else
       let grid, flashed = step grid in
-      print_grid (orig_n - n + 1) grid;
       run' (n_flashed + flashed) (n - 1) grid
   in
   run' 0 orig_n grid
 
 let () =
-  Aoc.zip Aoc.nat Aoc.stdin
-  |> Seq.fold_left add_to_map PosMap.empty
-  |> run 100
-  |> Printf.printf "%d\n"
+  let grid =
+    Aoc.zip Aoc.nat Aoc.stdin |> Seq.fold_left add_to_map PosMap.empty
+  in
+  let number_of_positions = PosMap.cardinal grid in
+  let part1 = run 100 grid in
+  let part2 = count_runs_until (( = ) number_of_positions) grid in
+  Printf.printf "Part 1: %d\n" part1;
+  Printf.printf "Part 2: %d\n" part2
