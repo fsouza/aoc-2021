5b69f9e36a3e0cda878cb3aadcc217532ea42c4c
francisco souza <fsouza@users.noreply.github.com>
2021-12-15 11:44:18 -0500

    day15: consolidate in one file

--- day15/day15.ml
+++ day15/day15.ml
@@ -20,7 +20,7 @@ let add_to_grid { map; _ } (row_idx, row) =
   let rows = String.length row in
   let map =
     row
-    |> Stdlib.String.to_seqi
+    |> String.to_seqi
     |> Seq.fold_left
          (fun map (col_idx, col) ->
            PosMap.add ~key:(row_idx, col_idx) ~data:(int_of_char col) map)
@@ -28,10 +28,10 @@ let add_to_grid { map; _ } (row_idx, row) =
   in
   { map; rows }
 
+(* Note: can make this fast with a priorityqueue. Do we need it for part 2? *)
 let distance (row_orig, col_orig) (row_dest, col_dest) =
   abs (row_dest - row_orig) + abs (col_dest - col_orig)
 
-(* Note: can make this fast with a priorityqueue. Do we need it for part 2? *)
 let next_pos open_set f_score =
   let lowest_pos, _ =
     PosSet.fold
@@ -50,7 +50,20 @@ let neighbor_positions (row, col) rows =
   |> List.filter ~f:(fun (row, col) ->
          row >= 0 && row < rows && col >= 0 && col < rows)
 
-let find_path { rows; map } =
+let non_zero_mod x y =
+  let v = x mod y in
+  if v = 0 then y else v
+
+let get_virtual_value { rows; map } (row, col) =
+  (* Note: this function doesn't check bounds, if you ask for the value at
+     position (1e9, 1e9) it'll do the math like if the grid was that large *)
+  let row_add = row / rows in
+  let col_add = col / rows in
+  let row = row mod rows in
+  let col = col mod rows in
+  non_zero_mod (PosMap.find (row, col) map + row_add + col_add) 9
+
+let find_path get_value rows grid =
   (* somewhat slow A*, adapted from
      https://en.wikipedia.org/wiki/A*_search_algorithm#Pseudocode *)
   let rec find_path' dest open_set g_score f_score =
@@ -69,7 +82,7 @@ let find_path { rows; map } =
                 |> Option.value ~default:max_int
               in
               let tentative_gscore =
-                PosMap.find v g_score + PosMap.find neighbor_pos map
+                PosMap.find v g_score + get_value grid neighbor_pos
               in
               if tentative_gscore >= neighbor_score then
                 (open_set, g_score, f_score)
@@ -92,8 +105,13 @@ let find_path { rows; map } =
   find_path' dest open_set g_score f_score
 
 let () =
-  Aoc.zip Aoc.nat Aoc.stdin
-  |> Seq.fold_left add_to_grid { rows = 0; map = PosMap.empty }
-  |> find_path
+  let grid =
+    Aoc.zip Aoc.nat Aoc.stdin
+    |> Seq.fold_left add_to_grid { rows = 0; map = PosMap.empty }
+  in
+  find_path (fun { map; _ } pos -> PosMap.find pos map) grid.rows grid
   |> Option.value ~default:max_int
-  |> Printf.printf "%d\n"
+  |> Printf.printf "Part 1: %d\n";
+  find_path get_virtual_value (grid.rows * 5) grid
+  |> Option.value ~default:max_int
+  |> Printf.printf "Part 2: %d\n"
