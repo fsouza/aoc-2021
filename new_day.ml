d4832c1ecc213022161e6ad6d8ef6981d74d3963
francisco souza <fsouza@users.noreply.github.com>
2021-12-16 12:57:30 -0500

    new_day: default to one module per day

--- new_day.ml
+++ new_day.ml
@@ -1,9 +1,11 @@
-let dune_file_content =
-  {|(executable
- (name part1)
+let dune_file_content day_number =
+  Printf.sprintf
+    {|(executable
+ (name day%d)
  (libraries aoc)
- (modules Part1))
-|}
+ (modules Day%d))
+|} day_number
+    day_number
 
 let last_day () =
   Sys.readdir Filename.current_dir_name
@@ -17,24 +19,26 @@ let day_number day =
   let start_pos = day_length - number_length in
   String.sub day start_pos number_length |> int_of_string
 
-let folder_name () =
-  last_day () |> day_number |> ( + ) 1 |> Printf.sprintf "day%02d"
-
 let write_file folder name contents =
   let file_path = Filename.concat folder name in
   let file = open_out file_path in
   output_string file contents;
   close_out file
 
-let write_dune_file folder = write_file folder "dune" dune_file_content
-let write_part1_module folder = write_file folder "part1.ml" ""
+let write_dune_file day_number folder =
+  write_file folder "dune" @@ dune_file_content day_number
+
+let write_module_file day_number folder =
+  write_file folder (Printf.sprintf "day%d.ml" day_number) ""
+
 let run_fmt () = Sys.command "dune build @fmt --auto-promote" |> ignore
 
 let () =
-  let folder = folder_name () in
+  let day_number = last_day () |> day_number |> ( + ) 1 in
+  let folder = Printf.sprintf "day%02d" day_number in
   let folder = Filename.concat Filename.current_dir_name folder in
   Sys.mkdir folder 0o755;
-  write_dune_file folder;
-  write_part1_module folder;
+  write_dune_file day_number folder;
+  write_module_file day_number folder;
   run_fmt ();
   Printf.printf "Successfully created '%s'\n" folder
