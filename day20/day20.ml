3bafba409f22651125b1331938b0f3885af76dd4
francisco souza <fsouza@users.noreply.github.com>
2021-12-20 12:12:41 -0500

    day 20 part 1

--- day20/day20.ml
+++ day20/day20.ml
@@ -9,7 +9,6 @@ module Pos = struct
     if row_cmp = 0 then Int.compare col1 col2 else row_cmp
 end
 
-module IntSet = Set.Make (Int)
 module PosSet = Set.Make (Pos)
 
 let value_of_pixel = function
@@ -17,38 +16,102 @@ let value_of_pixel = function
   | _ -> 0
 
 let int_of_pixels =
-  let rec int_of_pixels' acc = function
-    | [] -> acc
-    | hd :: tl -> int_of_pixels' ((acc * 2) + value_of_pixel hd) tl
+  let open Seq in
+  let rec int_of_pixels' acc seq =
+    match seq () with
+    | Nil -> acc
+    | Cons (v, seq) -> int_of_pixels' ((acc * 2) + value_of_pixel v) seq
   in
   int_of_pixels' 0
 
-type image = { grid : PosSet.t; width : int; height : int; infinite : char }
+type image = {
+  grid : PosSet.t;
+  row_bounds : int * int;
+  col_bounds : int * int;
+  infinite : char;
+}
 
 let initial_image =
-  { grid = PosSet.empty; width = 0; height = 0; infinite = '.' }
+  {
+    grid = PosSet.empty;
+    row_bounds = (0, 0);
+    col_bounds = (0, 0);
+    infinite = '.';
+  }
 
-let enhance algorithm { grid; infinite; width; height } = failwith "TODO"
+let all_positions
+    { row_bounds = min_row, max_row; col_bounds = min_col, max_col; _ } =
+  let open Seq in
+  let min_row = min_row - 2 in
+  let max_row = max_row + 2 in
+  let min_col = min_col - 2 in
+  let max_col = max_col + 2 in
+  let rec all_positions' row col () =
+    if row > max_row then Nil
+    else if col > max_col then all_positions' (row + 1) min_col ()
+    else Cons ((row, col), all_positions' row (col + 1))
+  in
+  ((min_row, max_row), (min_col, max_col), all_positions' min_row min_col)
+
+let is_inbound (min_row, max_row) (min_col, max_col) (row, col) =
+  row >= min_row && row <= max_row && col >= min_col && col <= max_col
+
+let get_pixel { grid; row_bounds; col_bounds; infinite } pos =
+  if is_inbound row_bounds col_bounds pos then
+    if PosSet.mem pos grid then '#' else '.'
+  else infinite
+
+let square_positions (center_row, center_col) =
+  let open Seq in
+  let rec square_positions' row col () =
+    if row > center_row + 1 then Nil
+    else if col > center_col + 1 then
+      square_positions' (row + 1) (center_col - 1) ()
+    else Cons ((row, col), square_positions' row (col + 1))
+  in
+  square_positions' (center_row - 1) (center_col - 1)
+
+let get_index image pos =
+  pos |> square_positions |> Seq.map (get_pixel image) |> int_of_pixels
+
+let enhance algorithm ({ infinite; _ } as image) =
+  let row_bounds, col_bounds, all_positions = all_positions image in
+  let grid =
+    all_positions
+    |> Seq.fold_left
+         (fun grid pos ->
+           let idx = get_index image pos in
+           let ch = algorithm.(idx) in
+           if ch = '#' then PosSet.add pos grid else grid)
+         PosSet.empty
+  in
+  let infinite_idx = value_of_pixel infinite * 9 in
+  let infinite = algorithm.(infinite_idx) in
+  { grid; row_bounds; col_bounds; infinite }
 
 let add_to_image image (row_idx, row) =
   row
   |> String.to_seqi
   |> Seq.fold_left
-       (fun ({ grid; width; height; _ } as image) (col_idx, col) ->
+       (fun ({
+               grid;
+               row_bounds = min_row, max_row;
+               col_bounds = min_col, max_col;
+               _;
+             } as image) (col_idx, col) ->
          let grid =
            if Char.equal col '#' then PosSet.add (row_idx, col_idx) grid
            else grid
          in
-         let width = max width (String.length row) in
-         let height = max height (row_idx + 1) in
-         { image with grid; width; height })
+         {
+           image with
+           grid;
+           row_bounds = (min_row, max max_row row_idx);
+           col_bounds = (min_col, max max_col col_idx);
+         })
        image
 
-let count_lit_pixels { grid; infinite; _ } =
-  if infinite = '#' then
-    failwith
-      "can't count infinite lit pixels, image is probably in an invalid state"
-  else PosSet.cardinal grid
+let count_lit_pixels { grid; _ } = PosSet.cardinal grid
 
 let () =
   let algorithm = read_line () |> String.to_seq |> Array.of_seq in
